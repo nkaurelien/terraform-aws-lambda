50c36e5a213908c1c9511d08957003c84ed0dde0
nkaurelien <nkaurelien@gmail.com>
2024-11-13 17:38:53 +0100

    remove unecessary  file main.0.tf

--- main.0.tf
+++
@@ -1,8 +0,0 @@
-
-# resource "aws_lambda_function" "this" {
-#   filename      = "lambda_function.zip"
-#   function_name = "example_lambda"
-#   role          = aws_iam_role.lambda_role.arn
-#   handler       = "index.handler"
-#   runtime       = "nodejs14.x"
-# }
